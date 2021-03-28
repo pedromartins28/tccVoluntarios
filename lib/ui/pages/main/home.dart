29206d99a0163d49610229070d7fdb2486bbbdb3
Gabriel Penido <38563539+LePenidon@users.noreply.github.com>
2021-03-28 15:52:10 -0300

    pt-br

--- lib/ui/pages/main/home.dart
+++ lib/ui/pages/main/home.dart
@@ -368,7 +368,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                         ),
                       ),
                       Text(
-                        "CLIQUE EM UM PARA VÊ-LO NO MAPA",
+                        "CLIQUE EM UMA PARA VÊ-LA NO MAPA",
                         textAlign: TextAlign.center,
                         style: TextStyle(
                           fontSize: 12.0,
